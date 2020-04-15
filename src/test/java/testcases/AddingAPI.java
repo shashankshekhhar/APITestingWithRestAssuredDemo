a1afedff4466a2fb3a405f551878ccc170fc0104
Shashank Shekhar <ShashankShekhar@gmail.com>
2020-04-15 12:47:27 +0530

    ThirdCommit

--- src/test/java/testcases/AddingAPI.java
+++ src/test/java/testcases/AddingAPI.java
@@ -1,8 +1,7 @@
 package testcases;
 import io.restassured.*;
-
+import org.hamcrest.Matchers.*;
 import static io.restassured.RestAssured.*;
-
 import org.junit.Test;
 
 import BDD.APITestProjectDemo.payload;
@@ -13,7 +12,8 @@ public class AddingAPI {
 	public void addplacetest() {
 		
 		RestAssured.baseURI="https://rahulshettyacademy.com";
-		given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json").body(payload.addPayload()).when().post("/maps/api/place/add/json").then().log().all().assertThat().statusCode(200);
+		given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json").body(payload.addPayload()).when().post("/maps/api/place/add/json").then().log().all().assertThat().statusCode(200)
+		.header("connection","Keep-Alive");
 		
 		
 		
